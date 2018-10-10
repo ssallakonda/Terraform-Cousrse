b1a1e3006d48eb99cc6b4a31ea926a6fc9811234
ssallakonda <santhosh_doll@yahoo.co.in>
2018-10-10 16:14:51 +0000

    modified:   vars.tf 	modified:   ../first-steps/instance.tf

--- demo-1/vars.tf
+++ demo-1/vars.tf
@@ -1,13 +1,13 @@
 variable "AWS_ACCESS_KEY" {}
 variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "eu-east-1"
 }
 variable "AMIS" {
   type = "map"
   default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-0d729a60"
+    us-east-1 = "ami-059eeca93cf09eebd"
+    us-west-2 = "ami-0e32ec5bc225539f5"
+    eu-west-1 = "ami-0ad16744583f21877"
   }
 }
