be23d01fc0fc81a421895b38e39630d8a676caea
ssallakonda <santhosh_doll@yahoo.co.in>
2018-10-10 16:15:34 +0000

    modified:   ../first-steps/instance.tf

--- first-steps/instance.tf
+++ first-steps/instance.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  access_key = "ACCESS_KEY_HERE"
-  secret_key = "SECRET_KEY_HERE"
+  access_key = "AKIAJSP4HDFSHO53BUCQ"
+  secret_key = "8RGBCda0s9fc9JBTN3htk8VKrNXZEJbTgsQJnjig"
   region     = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0d729a60"
+resource "aws_instance" "terraform-test" {
+  ami           = "ami-0ff8a91507f77f867"
   instance_type = "t2.micro"
 }
